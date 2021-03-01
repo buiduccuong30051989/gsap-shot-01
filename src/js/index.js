eff03914d5d2328370d182c4808f5fa19930c8ef
Cuong <bui.duc.cuong@sun-asterisk.com>
2021-03-01 20:17:24 +0700

    Animating Caption, Mobile Menu

--- src/js/index.js
+++ src/js/index.js
@@ -49,10 +49,48 @@ function animateControl() {
   });
 }
 
+function animateCaption() {
+  gsap.from('.js-caption', {
+    opacity: 0,
+    delay: 2,
+    y: 30,
+    stagger: 0.4,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  });
+}
+
+const animateMenuItem = gsap.to('.js-nav-item-mobile', {
+  opacity: 1,
+  scale: 1,
+  y: 0,
+  paused: true,
+  stagger: {
+    amount: 0.5,
+  },
+});
+
+const animateHeaderMenu = gsap.to('.js-header', {
+  height: '100%',
+  duration: 0.5,
+  reversed: true,
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   animateLogo();
   if (!isMobile()) {
     animateNav();
   }
   animateControl();
+  animateCaption();
+
+  const burger = document.querySelector('.js-btn-mobile-menu');
+  const header = document.querySelector('.js-header');
+
+  burger.onclick = function () {
+    header.classList.toggle('menu-opened');
+    animateHeaderMenu.reversed()
+      ? animateHeaderMenu.play() && animateMenuItem.play()
+      : animateHeaderMenu.reverse() && animateMenuItem.reverse();
+  };
 });
